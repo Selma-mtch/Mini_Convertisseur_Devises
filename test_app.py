497c5c035319e9e4235268db3eae7abca9551774
Selma-mtch <mataiche.selma77@gmail.com>
2026-06-25 18:36:09 +0200

    Ajout questionreflexion

--- test_app.py
+++ test_app.py
@@ -1,16 +1,16 @@
-import pytest
-
 from backend.app_functions import rates, convert, format_history
-
 
 def test_convert_usd_to_eur():
     result = convert(11, "USD", "EUR", rates)
     assert result == 10
 
 
+
 def test_convert_eur_to_usd():
     result = convert(10, "EUR", "USD", rates)
     assert result == 11
+
+
 
 
 def test_convert_returns_none_when_amount_is_zero():
@@ -19,6 +19,15 @@ def test_convert_returns_none_when_amount_is_zero():
 
 
 def test_convert_returns_none_when_amount_is_negative():
+    result = convert(-5, "EUR", "USD", rates)
+    assert result is None
+
+def test_convert_with_zero_amount_returns_none():
+    result = convert(0, "EUR", "USD", rates)
+    assert result is None
+
+
+def test_convert_with_negative_amount_returns_none():
     result = convert(-5, "EUR", "USD", rates)
     assert result is None
 
